5b3ea5078d0028ce587c5bad00b6186a3c47e234
Lucian <la@checkmate.dk>
2024-09-03 12:13:14 +0200

    98% done

--- philosiphers.go
+++ philosiphers.go
@@ -7,30 +7,48 @@ import (
 
 func eat(id int, eatCounter int) {
 	time.Sleep(2 * time.Second)
-	fmt.Println("Philosopher: %d is eating for the %d time", id, eatCounter)
+	fmt.Printf("Philosopher: %d is eating for the %d time", id, eatCounter)
 }
 
-func thinking(id int) {
-	time.Sleep(2 * time.Second)
-	fmt.Println("Philosopher: %d is thinking", id)
+func think(id int, forks chan [5]bool) {
+	eatCounter := 0
+	for {
+		time.Sleep(2 * time.Second)
+		fmt.Printf("Philosopher: %d is thinking...", id)
+		leftFork := isAvailable(id, forks)
+		rightFork := isAvailable((id+1)%5, forks)
+		if leftFork && rightFork {
+			setAvailable(id, forks)
+			setAvailable((id+1)%5, forks)
+			eat(id, eatCounter)
+			eatCounter++
+			setAvailable(id, forks)
+			setAvailable((id+1)%5, forks)
+		}
+	}
 }
 
-func philisophise(id int, forks chan int){
-	
+func isAvailable(id int, forks chan [5]bool) bool {
+	myForks := <-forks
+	availability := myForks[id]
+	forks <- myForks
+	return availability
+}
+
+func setAvailable(id int, forks chan [5]bool) {
+	myForks := <-forks
+	myForks[id] = !myForks[id]
+	forks <- myForks
 }
 
 func main() {
-   // Creating a channel
-   channel := make(chan int)
+	// Creating a channel
+	channel := make(chan [5]bool)
+	channel <- [5]bool{true, true, true, true, true}
 
-   // Creating 10.000 workers to execute the task
-   for i := 0; i < 5; i++ {
-      go philisophise(i, channel)
-   }
-
-   // Filling channel with 100.000 numbers to be executed
-   for i := 0; i < 100000; i++ {
-      channel <- i
-   }
+	// Creating 10.000 workers to execute the task
+	for i := 0; i < 5; i++ {
+		go think(i, channel)
+	}
 
 }
