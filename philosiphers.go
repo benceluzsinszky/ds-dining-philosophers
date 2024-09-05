cd193a66fdc57fc675b3bab50232af08e4a388b5
bence <benceluzsinszky@gmail.com>
2024-09-05 18:58:29 +0200

    add locks, and buffer to forks channel

--- philosiphers.go
+++ philosiphers.go
@@ -2,33 +2,38 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var mutex sync.Mutex
+
 func eat(id int, eatCounter int) {
 	time.Sleep(2 * time.Second)
-	fmt.Printf("Philosopher: %d is eating for the %d time", id, eatCounter)
+	fmt.Printf("Philosopher: %d is eating for the %d time\n", id, eatCounter)
 }
 
 func think(id int, forks chan [5]bool) {
 	eatCounter := 0
 	for {
-		time.Sleep(2 * time.Second)
-		fmt.Printf("Philosopher: %d is thinking...", id)
 		leftFork := isAvailable(id, forks)
 		rightFork := isAvailable((id+1)%5, forks)
 		if leftFork && rightFork {
 			setAvailable(id, forks)
 			setAvailable((id+1)%5, forks)
-			eat(id, eatCounter)
 			eatCounter++
+			eat(id, eatCounter)
 			setAvailable(id, forks)
 			setAvailable((id+1)%5, forks)
 		}
+		time.Sleep(1 * time.Second)
+		fmt.Printf("Philosopher: %d is thinking...\n", id)
 	}
 }
 
 func isAvailable(id int, forks chan [5]bool) bool {
+	mutex.Lock()
+	defer mutex.Unlock()
 	myForks := <-forks
 	availability := myForks[id]
 	forks <- myForks
@@ -36,19 +41,21 @@ func isAvailable(id int, forks chan [5]bool) bool {
 }
 
 func setAvailable(id int, forks chan [5]bool) {
+	mutex.Lock()
+	defer mutex.Unlock()
 	myForks := <-forks
 	myForks[id] = !myForks[id]
 	forks <- myForks
 }
 
 func main() {
-	// Creating a channel
-	channel := make(chan [5]bool)
-	channel <- [5]bool{true, true, true, true, true}
+	forks := make(chan [5]bool, 2)
+	forks <- [5]bool{true, true, true, true, true}
 
-	// Creating 10.000 workers to execute the task
 	for i := 0; i < 5; i++ {
-		go think(i, channel)
+		go think(i, forks)
 	}
+
+	time.Sleep(30 * time.Second)
 
 }
